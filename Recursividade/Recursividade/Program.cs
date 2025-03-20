1a856d11689d1f4c73e206e4585df6bf91526e18
JoseGhu <69773420+JoseGhu@users.noreply.github.com>
2025-03-20 13:57:29 -0300

    feito por Upload

---
+++ Recursividade/Recursividade/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Sequência de Fibonacci até 50.000:");
+
+        int i = 0;
+        while (true)
+        {
+            int fibValor = Fibonacci(i);
+            if (fibValor > 50000)
+                break;
+
+            Console.WriteLine(fibValor);
+            i++;
+        }
+    }
+
+    static int Fibonacci(int numero)
+    {
+        if (numero == 0)
+            return 0;
+        if (numero == 1)
+            return 1;
+
+        return Fibonacci(numero - 1) + Fibonacci(numero - 2);
+    }
+}
